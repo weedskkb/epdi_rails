57e7eb10703bd55248faafce27c14790ceca3015
shin <nakajimabb@gmail.com>
2025-10-14 13:00:09 +0900

    取込データ一覧 エラー修正

--- app/models/trn_capture_data.rb
+++ app/models/trn_capture_data.rb
@@ -23,4 +23,39 @@ class TrnCaptureData < ApplicationRecord
   belongs_to :capture_history, class_name: "TrnCaptureHistory", foreign_key: "CAPTURE_HISTORY_NO"
   belongs_to :department, class_name: "Department", foreign_key: "DEPARTMENT_NO"
   belongs_to :company, class_name: "Company", foreign_key: "COMPANY_NO"
+
+  delegate :capture_category, to: :capture_history, allow_nil: true
+
+  def summary
+    template = capture_category&.abstract
+    return nil if template.blank?
+
+    text = template.dup
+    text.gsub!("発生月", formatted_month(capture_history&.accrual_month || capture_history&.payment_month)) if text.include?("発生月")
+    text.gsub!("支払月", formatted_month(capture_history&.payment_month)) if text.include?("支払月")
+    text.gsub!("店舗名", department&.department_name.to_s) if text.include?("店舗名")
+    text.gsub!("会社名", company&.company_name.to_s) if text.include?("会社名")
+    text.gsub!("仕入先別", capture_category&.supplier_abstract.to_s) if text.include?("仕入先別")
+    text
+  end
+
+  private
+
+  def formatted_month(value)
+    return "" if value.blank?
+
+    date =
+      case value
+      when Date
+        value
+      when Time, DateTime
+        value.to_date
+      else
+        Date.parse(value.to_s)
+      end
+
+    date.strftime("%-m月")
+  rescue ArgumentError, TypeError
+    value.to_s
+  end
 end
--- app/services/payment_data/finder.rb
+++ app/services/payment_data/finder.rb
@@ -8,7 +8,7 @@ module PaymentData
     end
 
     def all
-      scope = TrnCaptureData.includes(:department, :company)
+      scope = TrnCaptureData.includes(:department, :company, capture_history: :capture_category)
       scope = scope.where(COMPANY_NO: params[:company_no]) if params[:company_no].present?
       scope.order(:CAPTURE_HISTORY_NO, :ROW_NO)
     end
