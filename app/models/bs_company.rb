5ee1f7565f512112ab8c45e1f302186a9d89be6c
shin <nakajimabb@gmail.com>
2025-10-29 12:55:36 +0900

    会社存在チェックバッチ

---
+++ app/models/bs_company.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class BsCompany < ApplicationRecord
+  scope :active, -> { where(delete_flg: false) }
+
+  belongs_to :business_connection,
+             class_name: "BusinessConnection",
+             foreign_key: :business_connection_code,
+             primary_key: :code,
+             optional: true
+  has_many :departments, foreign_key: :company_id, inverse_of: :company
+  has_many :capture_records, class_name: "TrnCaptureRecord", foreign_key: :company_id, inverse_of: :company
+  has_many :journal_entry_patterns, class_name: "JournalEntryPattern", foreign_key: :company_id, inverse_of: :company
+  has_many :journal_entry_records, class_name: "TrnJournalEntryRecord", foreign_key: :company_id, inverse_of: :company
+
+  alias_method :capture_data, :capture_records
+  alias_method :capture_data=, :capture_records=
+  alias_method :journal_entry_data, :journal_entry_records
+  alias_method :journal_entry_data=, :journal_entry_records=
+end
